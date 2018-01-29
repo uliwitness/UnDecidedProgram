ff62be4b7fe334d5618df8b7ab042abc919169f3
Uli Kusterer <ulivc@zathras.de>
2018-01-29 03:35:37 +0100

    Add a target for an animation editor.

---
+++ UnDecidedEditor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UnDecidedEditor
+//
+//  Created by Uli Kusterer on 29.01.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ UnDecidedEditor/AppDelegate.m
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.m
+//  UnDecidedEditor
+//
+//  Created by Uli Kusterer on 29.01.18.
+//  Copyright © 2018 Uli Kusterer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@property (weak)
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching: (NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+
+-(void)	applicationWillTerminate: (NSNotification *)aNotification
+{
+	// Insert code here to tear down your application
+}
+
+
+-(BOOL)	application: (NSApplication *)sender openFile: (NSString *)filename
+{
+	
+}
+
+@end
