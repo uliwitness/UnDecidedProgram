cf1aa9ee57445ae2924887561946723048ed3fe0
Uli Kusterer <ulivc@zathras.de>
2018-02-16 10:32:52 +0100

    Better file UI in editor.
    We can now "save as", the recents menu works, and the title bar is updated when we have a document.

--- UnDecidedEditor/UnDecidedEditorAppDelegate.m
+++ UnDecidedEditor/UnDecidedEditorAppDelegate.m
@@ -43,13 +43,15 @@
 	_editView.characterImage = self.characterImage;
 	[_editView setNeedsDisplay: YES];
 	
+	[self.window setTitleWithRepresentedFilename:self.characterImagePath];
+
 	return YES;
 }
 
 
 -(BOOL) validateMenuItem:(NSMenuItem *)menuItem
 {
-	if( menuItem.action == @selector(saveDocument:) )
+	if( menuItem.action == @selector(saveDocument:) || menuItem.action == @selector(saveDocumentAs:) )
 	{
 		return self.characterImagePath != nil;
 	}
@@ -66,6 +68,32 @@
 }
 
 
+-(IBAction)	saveDocumentAs: (id)sender
+{
+	if( !self.characterImagePath ) return;
+	
+	NSSavePanel * savePanel = [NSSavePanel savePanel];
+	NSModalResponse response = [savePanel runModal];
+	if( response == NSModalResponseOK )
+	{
+		NSString * newPath = savePanel.URL.path;
+		
+		NSError * error = nil;
+		if( ![[NSFileManager defaultManager] copyItemAtPath: self.characterImagePath toPath: newPath error: &error] )
+		{
+			[[NSApplication sharedApplication] presentError: error];
+			return;
+		}
+		
+		self.characterImagePath = newPath;
+		[self.characterImage writeToDirectory: self.characterImagePath];
+		[[NSDocumentController sharedDocumentController] noteNewRecentDocumentURL: savePanel.URL];
+		[self.window setTitleWithRepresentedFilename:savePanel.URL.path];
+	}
+
+}
+
+
 -(IBAction)	openDocument: (id)sender
 {
 	NSOpenPanel * openPanel = [NSOpenPanel openPanel];
@@ -75,7 +103,9 @@
 	NSModalResponse response = [openPanel runModal];
 	if( response == NSModalResponseOK )
 	{
-		[self application: [NSApplication sharedApplication] openFile: openPanel.URLs.firstObject.path];
+		NSURL * urlToOpen = openPanel.URLs.firstObject;
+		[[NSDocumentController sharedDocumentController] noteNewRecentDocumentURL: urlToOpen];
+		[self application: [NSApplication sharedApplication] openFile: urlToOpen.path];
 	}
 }
 
